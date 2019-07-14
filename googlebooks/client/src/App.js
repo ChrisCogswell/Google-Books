dd524ffcf4c40ff5c1df1aad5856acdbf77c74a4
ChrisCogs <ccogs@yahoo.com>
2019-07-13 21:08:05 -0400

    Added Saved and Search pages. Updated some css.

--- googlebooks/client/src/App.js
+++ googlebooks/client/src/App.js
@@ -1,24 +1,25 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import Books from "./pages/Books";
-import Detail from "./pages/Detail";
-import NoMatch from "./pages/NoMatch";
-import Nav from "./components/Nav";
+import Search from "./pages/Search";
+import Saved from "./pages/Saved";
+import NavTabs from "./components/NavTabs";
+import "./App.css";
 
-function App() {
-  return (
-    <Router>
+class App extends Component {
+  render() {
+    return (
+      <Router>
       <div>
-        <Nav />
+        <NavTabs/>
         <Switch>
-          <Route exact path="/" component={Books} />
-          <Route exact path="/books" component={Books} />
-          <Route exact path="/books/:id" component={Detail} />
-          <Route component={NoMatch} />
+          <Route exact path="/" component={Search} />
+          <Route exact path="/search" component={Search} />
+          <Route exact path="/saved" component={Saved} />
         </Switch>
       </div>
     </Router>
-  );
+    );
+  }
 }
 
 export default App;
